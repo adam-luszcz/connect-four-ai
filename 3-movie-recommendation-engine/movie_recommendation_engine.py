979f6932d6cdf99a0407c343c7c09457058a0b7e
Adam Łuszcz <adam.luszcz20@gmail.com>
2023-11-16 22:09:36 +0100

    Add description

--- 3-movie-recommendation-engine/movie_recommendation_engine.py
+++ 3-movie-recommendation-engine/movie_recommendation_engine.py
@@ -1,3 +1,26 @@
+"""
+The program is a movie recommendation engine.
+It uses the Surprise library to train a recommendation model and provide movie recommendations for a specific user.
+The program reads from an Excel file, handles missing values, and transforms the data into a format where each row represents a user, a movie, and a rating.
+It uses the Pearson and Cosine metrics to calculate the distance between users.
+The output is top 5 movie recommendations and 5 movies not recommended for a specific user.
+
+
+How to set up
+---
+Install the packages from the requirements.txt with the following command pip3 install -r requirements.txt
+
+
+How to run
+---
+Run the program with the following command python3 movie_recommendation_engine.py
+You will be asked to provide a user for whom the recommendations are to be generated.
+Please type in user name exactly as it is in the Excel file.
+The program will print top 5 movie recommendations and 5 movies not recommended with both the Pearson and Cosine metrics.
+
+
+Authors: Adam Łuszcz, Anna Rogala
+"""
 import pandas as pd
 from surprise import Reader, Dataset
 from surprise.model_selection import train_test_split
